a348b984d5ae4a9e661a32c1a1815afc0d9bee3e
Alexander <alykoshin@gmail.com>
2016-04-27 20:38:59 +0300

    fixed invalid parsing of content-disposition header

--- examples/download-binaries.js
+++ examples/download-binaries.js
@@ -12,12 +12,15 @@ var down = require('../')({ dest: 'tmp', progress: true, overwrite: false });
 
 var links = [
   //{ url: 'https://nodejs.org/dist/v4.2.6/win-x64/node.exe', path: 'v4.2.6/win-x64', name: 'node.exe' },
-  { url: 'https://nodejs.org/dist/v4.2.6/win-x64/node.lib', path: 'v4.2.6/win-x64' },
+  //{ url: 'https://nodejs.org/dist/v4.2.6/win-x64/node.lib', path: 'v4.2.6/win-x64' },
   //{ url: 'https://nodejs.org/dist/v4.2.6/win-x86/node.exe', path: 'v4.2.6/win-x86' },
   //{ url: 'https://nodejs.org/dist/v4.2.6/win-x86/node.lib', path: 'v4.2.6/win-x86' },
   //{ url: 'https://nodejs.org/dist/npm/npm-1.4.9.zip',       path: 'npm',            name: 'npm-1.4.9.zip' },
-  { url: 'https://nodejs.org/dist/npm/npm-1.4.9.zip',       path: 'npm' },
+  //{ url: 'https://nodejs.org/dist/npm/npm-1.4.9.zip',       path: 'npm' },
   //{ url: 'https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe', path: 'nuget', name: 'nuget.exe' }
+  //{ url: 'https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe', path: 'nuget' }
+  //{ url: 'https://repo.jenkins-ci.org/releases/com/sun/winsw/winsw/1.18/winsw-1.18-bin.exe', path: 'winsw', name: 'winsw-1.18-bin.exe' }
+  { url: 'https://repo.jenkins-ci.org/releases/com/sun/winsw/winsw/1.18/winsw-1.18-bin.exe', path: 'winsw' }
 ];
 
 
