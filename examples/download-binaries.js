48a63e8a2b9fe020e6ea6defc17d3ce4cb3d829d
Alexander <alykoshin@gmail.com>
2016-08-09 11:06:52 +0300

    fixed error handling

--- examples/download-binaries.js
+++ examples/download-binaries.js
@@ -7,7 +7,7 @@
 var chalk = require('chalk');
 
 //var spinner = require('text-spinner')();
-var down = require('../')({ dest: 'tmp', progress: true, overwrite: false });
+var downAll = require('../')({ dest: 'tmp', progress: true, overwrite: false });
 
 
 var links = [
@@ -21,72 +21,92 @@ var links = [
   //{ url: 'https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe', path: 'nuget' }
   //{ url: 'https://repo.jenkins-ci.org/releases/com/sun/winsw/winsw/1.18/winsw-1.18-bin.exe', path: 'winsw', name: 'winsw-1.18-bin.exe' }
   {
+    name: 'winsw.exe',
     path: 'winsw',
     url: 'https://repo.jenkins-ci.org/releases/com/sun/winsw/winsw/1.18/winsw-1.18-bin.exe'
-  },
-  {
-    path: 'npm',
-    urls: [
-      'https://nodejs.org/dist/npm/npm-1.4.9-.zip',
-      'https://nodejs.org/dist/npm/npm-1.4.8-.zip',
-      'https://nodejs.org/dist/npm/npm-1.4.7.zip',
-    ]
-  },
+  }, {
+    path: 'v4.2.6/win-x64',
+    url: 'https://nodejs.org/dist/v4.2.6/win-x64/node.exe'
+  }, {
+    path: 'v4.2.6/win-x86',
+    url: 'https://nodejs.org/dist/v4.2.6/win-x86/node.exe'
+  }, {
+    path: 'nuget',
+    url: 'https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe'
+  }, {
+    path: 'nssm', url: 'https://nssm.cc/release/nssm-2.24.zip'
+  }
+  /*
+   {
+   path: 'winsw',
+   url: 'https://repo.jenkins-ci.org/releases/com/sun/winsw/winsw/1.18/winsw-1.18-bin.exe'
+   },
+   {
+   path: 'npm',
+   urls: [
+   'https://nodejs.org/dist/npm/npm-1.4.9-.zip',
+   'https://nodejs.org/dist/npm/npm-1.4.8-.zip',
+   'https://nodejs.org/dist/npm/npm-1.4.7.zip',
+   ]
+   },
+   */
 ];
 
 
 // Downloader object level events
 
-down.on('add', function(progressObj) {
-  console.log(chalk.green('# down.on(\'add\') progressObj:' + JSON.stringify(progressObj)));
+downAll.on('add', function(progressObj) {
+  console.log(chalk.green('# downAll.on(\'add\') progressObj:' + JSON.stringify(progressObj)));
 });
 
-down.on('start', function(progressObj) {
-  console.log(chalk.green('# own.on(\'start\') progressObj:' + JSON.stringify(progressObj)));
+downAll.on('start', function(progressObj) {
+  console.log(chalk.green('# downAll.on(\'start\') progressObj:' + JSON.stringify(progressObj)));
 });
 
-down.on('progress', function(/*progressObj*/) {
+downAll.on('progress', function(/*progressObj*/) {
   //spinner.spin();
 });
 
-down.on('end', function(progressObj) {
-  console.log(chalk.green('## down.on(\'end\') progressObj:' + JSON.stringify(progressObj)));
+downAll.on('end', function(progressObj) {
+  console.log(chalk.green('## downAll.on(\'end\') progressObj:' + JSON.stringify(progressObj)));
 });
 
-down.on('error', function(err, linkObj) {
-  console.log(chalk.green('# down.on(\'error\'): err: ', err, '; linkObj: ' + JSON.stringify(linkObj)));
+downAll.on('error', function(err, linkObj) {
+  console.log(chalk.red('# downAll.on(\'error\'): err: ', err, '; linkObj: ' + JSON.stringify(linkObj)));
 });
 
 
 // File level events
 
-down.on('file-add', function(linkObj) {
-  console.log(chalk.blue('* down.on(\'file-add\'): linkObj:' + JSON.stringify(linkObj)));
+downAll.on('file-add', function(linkObj) {
+  console.log(chalk.blue('* downAll.on(\'file-add\'): linkObj:' + JSON.stringify(linkObj)));
 });
 
-down.on('file-start', function(linkObj, progressObj) {
-  console.log(chalk.blue('* down.on(\'file-start\'): linkObj:' + JSON.stringify(linkObj) + '; progressObj:' + JSON.stringify(progressObj)));
+downAll.on('file-start', function(linkObj, progressObj) {
+  console.log(chalk.blue('* downAll.on(\'file-start\'): linkObj:' + JSON.stringify(linkObj) + '; progressObj:' + JSON.stringify(progressObj)));
 });
 
-//down.on('file-progress', function(progressObj) {
+//downAll.on('file-progress', function(progressObj) {
 //  spinner.spin();
 //});
 
-down.on('file-end', function(linkObj, progressObj) {
-  console.log(chalk.blue('* down.on(\'file-end\'): linkObj:' + JSON.stringify(linkObj) + '; progressObj:' + JSON.stringify(progressObj)));
+downAll.on('file-end', function(linkObj, progressObj) {
+  console.log(chalk.blue('* downAll.on(\'file-end\'): linkObj:' + JSON.stringify(linkObj) + '; progressObj:' + JSON.stringify(progressObj)));
 });
 
-down.on('file-error', function(err, linkObj) {
-  console.log(chalk.blue('* down.on(\'error\'): err: ', err, '; linkObj: ' + JSON.stringify(linkObj)));
+downAll.on('file-error', function(err, linkObj) {
+  console.log(chalk.red('* downAll.on(\'file-error\'): err: ', err, '; linkObj: ' + JSON.stringify(linkObj)));
 });
 
 
 // Pass the object with file links and start download
 
-down.load(links, function(err) {
-  console.log('down.load(links, callback): callback() called');
+console.log(chalk.yellow('downAll.load(): start'));
+downAll.load(links, function(err) {
   if (err) {
-    console.log(chalk.red('down.load() returned err:', err));
+    console.log(chalk.red('downAll.load(): finish: err:', err));
+  } else {
+    console.log(chalk.yellow('downAll.load(): finish'));
   }
 });
 
